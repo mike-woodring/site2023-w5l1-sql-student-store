1b02d4454b13b440d4a4fac5d4d5d8c18cb861e7
Mike Woodring <mike.woodring@codepath.org>
2023-05-02 11:44:41 -0600

    Completed step 1.

---
+++ student_store_backend/student-store.sql
@@ -0,0 +1,8 @@
+\echo 'Delete and recreate student_store db?'
+\prompt 'Return for yes or control-C to cancel > ' answer
+
+DROP DATABASE student_store;
+CREATE DATABASE student_store;
+\connect student_store
+
+\i student-store-schema.sql
